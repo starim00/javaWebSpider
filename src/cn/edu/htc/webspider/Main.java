492edede475ac16820e725be2b2f4221f25e9ec4
starim00 <a517671982@qq.com>
2017-08-06 23:01:14 +0800

    2017-8-6 23:00:56
    抓取论坛全部的帖子，未解决需要登录的问题

---
+++ src/cn/edu/htc/webspider/Main.java
@@ -0,0 +1,8 @@
+package cn.edu.htc.webspider;
+
+public class Main {
+    public static void main(String argc[]){
+        WebSpider test = new WebSpider();
+        test.myPrint("https://rm93.com/thread.php?fid=54");
+    }
+}
